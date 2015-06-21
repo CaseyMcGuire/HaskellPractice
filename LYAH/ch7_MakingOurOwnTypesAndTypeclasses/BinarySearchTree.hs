cac3a91016838b72ed841ff4e6bb539639870ad9
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-06-21 14:00:51 -0700

    Did some examples from book

---
+++ LYAH/ch7_MakingOurOwnTypesAndTypeclasses/BinarySearchTree.hs
@@ -0,0 +1,32 @@
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+createTree :: (Ord a) => [a] -> Tree a
+createTree lst = foldr treeInsert EmptyTree lst
+
+instance Functor Tree where
+  fmap f EmptyTree = EmptyTree
+  fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
+
+main = do
+  let
+    tree = createTree [1,2,3,4,5,6]
+  print tree
+  return ()
